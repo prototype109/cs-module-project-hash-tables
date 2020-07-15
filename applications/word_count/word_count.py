5edf9567aacb8b64f26f57d60481ecfa25278c29
Brandon Nichols <brandonaronnichols@gamil.com>
2020-07-15 18:37:04 -0500

    completed word_count.py

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,4 +1,21 @@
 def word_count(s):
+    string_count = {}
+    split_string = s.split()
+    temp_word = ''
+    banned_characters = '":;,.-+=/\\|[]{}()*^&'
+
+    for index, word in enumerate(split_string):
+        temp_word = ''
+        for char in word:
+            if char not in banned_characters:
+                temp_word += char
+        if len(temp_word) > 0:    
+            if temp_word.lower() in string_count:
+                string_count[temp_word.lower()] += 1
+            else:
+                string_count[temp_word.lower()] = 1
+
+    return string_count
     # Your code here
 
 
